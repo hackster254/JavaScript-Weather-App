4a1e251d6b4ebd543e070d142140a8a7eb939b66
JuanGongora <gongora.animations@gmail.com>
2018-09-13 21:37:12 -0400

    add elements.js file with constants for TBD actions

---
+++ elements.js
@@ -0,0 +1,10 @@
+export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
+export const ELEMENT_SEARCH_CITY = document.getElementById('#city');
+
+export const ELEMENT_LOADING_TEXT = document.getElementById('#load');
+export const ELEMENT_WEATHER_BOX = document.getElementById('#weather');
+
+export const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
+export const ELEMENT_WEATHER_DESCRIPTION = document.getElementById('#weatherDescription');
+export const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
+
