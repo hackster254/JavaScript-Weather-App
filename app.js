9e1765629d922d06a0c1000ea618a7d0d54f7aa7
JuanGongora <gongora.animations@gmail.com>
2018-09-13 22:15:06 -0400

    revise const to be in app.js as export was not working

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('Hello');
+const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
+const ELEMENT_SEARCH_CITY = document.querySelector('#city');
+
+const ELEMENT_LOADING_TEXT = document.querySelector('#load');
+const ELEMENT_WEATHER_BOX = document.querySelector('#weather');
+
+const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
+const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
+const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
+
+ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
+
+function searchWeather() {
+    alert('this works!');
+}
--- elements.js
+++ elements.js
@@ -1,10 +1,9 @@
 export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
-export const ELEMENT_SEARCH_CITY = document.getElementById('#city');
+export const ELEMENT_SEARCH_CITY = document.querySelector('#city');
 
-export const ELEMENT_LOADING_TEXT = document.getElementById('#load');
-export const ELEMENT_WEATHER_BOX = document.getElementById('#weather');
+export const ELEMENT_LOADING_TEXT = document.querySelector('#load');
+export const ELEMENT_WEATHER_BOX = document.querySelector('#weather');
 
 export const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
-export const ELEMENT_WEATHER_DESCRIPTION = document.getElementById('#weatherDescription');
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
 export const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
-
