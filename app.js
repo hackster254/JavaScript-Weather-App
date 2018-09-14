6fc121e9a7a978c685c854d1d768a8c1ae212706
JuanGongora <gongora.animations@gmail.com>
2018-09-13 22:34:01 -0400

    revise searchWeather function for accepting cities

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
-const ELEMENT_SEARCH_CITY = document.querySelector('#city');
+const ELEMENT_SEARCH_CITY = document.getElementById('city');
 
-const ELEMENT_LOADING_TEXT = document.querySelector('#load');
-const ELEMENT_WEATHER_BOX = document.querySelector('#weather');
+const ELEMENT_LOADING_TEXT = document.getElementById('load');
+const ELEMENT_WEATHER_BOX = document.getElementById('weather');
 
 const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
-const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
+const ELEMENT_WEATHER_DESCRIPTION = document.getElementById('weatherDescription');
 const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
 
 ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    alert('this works!');
+    const CITY_NAME = ELEMENT_SEARCH_CITY.value.trim();
+    if (CITY_NAME.length === 0) {
+        return alert("You did not enter a city name");
+    }
+    alert(CITY_NAME);
 }
